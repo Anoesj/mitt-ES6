0455de8b5982aff3b81aa2208dab348dc2d9a6ef
Anoesj Sadraee <anoesjsadraee@gmail.com>
2017-12-10 21:51:06 +0100

    No more regexps, you can now use starts-with and ends-width instead, which are less heavy

--- mitt.js
+++ mitt.js
@@ -3,69 +3,72 @@
   *
   * Changes (by Anoesj Sadraee):
   * - Ported to a true ES6 module, so use 'new mitt()' now.
-  * - Full wildcard support (e.g. 'debug*' matches 'debug' and 'debug-verbose'). Turn off by initializing mitt with 'new mitt(false)'.
+  * - 'start-with' and 'ends-with' support (e.g. 'starts-with:debug' matches events named 'debug' and 'debug-verbose').
   ------------------------------------------------------------- **/
 export default class mitt {
   
-  constructor (fullWildcardsSupport = true) {
-    this.fullWildcardsSupport = fullWildcardsSupport;
+  constructor () {
     this.handlers = new Map();
-    if (this.fullWildcardsSupport) this.wildcardHandlers = new Map();
+    this.specialHandlers = new Map();
   }
 
   /** --------------------------------------------------------------
-    * Register an event handler for the given type.
-    * @param  {String} type:        Type of event to listen for. Can contain wildcard ('*').
-    * @param  {Function} handler:   Function to call in response to given event
+    * Register an event handler for the given event name.
+    * @param {String} eventName: Name of event to listen for. Can begin with 'starts-with' or 'end-with' to match grouped events.
+    * @param {Function} handler: Function to call in response to given event
     ------------------------------------------------------------- **/
-  on (type, handler) {
-    if (typeof type !== 'string' || typeof handler !== 'function') return;
+  on (eventName, handler) {
+    if (typeof eventName !== 'string' || typeof handler !== 'function') return;
 
-    // Save types with wildcards as a RegExp
-    if (this.fullWildcardsSupport && type.includes('*')) {
-      this.wildcardHandlers.set(type, {
-        regexp: new RegExp(`^${type.replace(/\*/g, '.*')}$`, 'ig'),
-        handler: handler
+    if (eventName.startsWith('starts-with:')) {
+      this.specialHandlers.set(eventName, {
+        handler: handler,
+        test: typeToTest => typeToTest.startsWith(eventName.substring(12))
+      });
+    }
+
+    else if (eventName.startsWith('ends-with:')) {
+      this.specialHandlers.set(eventName, {
+        handler: handler,
+        test: typeToTest => typeToTest.endsWith(eventName.substring(10))
       });
     }
 
     else {
-      this.handlers.set(type, handler);
+      this.handlers.set(eventName, handler);
     }
   }
 
   /** --------------------------------------------------------------
     * Remove an event handler for the given type.
-    * @param  {String} type:  Type of event to unregister handler from. Can contain wildcard ('*').
+    * @param {String} eventName: Name of event to unregister (special) handler from.
     ------------------------------------------------------------- **/
-  off (type) {
-    if (typeof type !== 'string') return;
-    this.handlers.delete(type);
-    if (this.fullWildcardsSupport) this.wildcardHandlers.delete(type);
+  off (eventName) {
+    if (typeof eventName !== 'string') return;
+    this.handlers.delete(eventName);
+    this.specialHandlers.delete(eventName);
   }
 
   /** --------------------------------------------------------------
     * Invoke all handlers for the given type.
-    * If present, wildcard handlers (containing a '*') are invoked after type-matched handlers.
-    * @param  {String} type:        The event type to invoke
-    * @param  {Any} fn (optional):  Any value (object is recommended and powerful), passed to each handler
+    * If present, special handlers (starting with 'starts-with' or 'ends-with') and wildcard handlers (containing a '*') are invoked after type-matched handlers.
+    * @param {String} eventName:  Name of the event to invoke
+    * @param {Any} fn (optional): Any value (object is recommended and powerful), passed to each handler
     ------------------------------------------------------------- **/
-  emit (type, fn) {
-    if (typeof type !== 'string') return;
+  emit (eventName, fn) {
+    if (typeof eventName !== 'string') return;
     
     // Fire regular handler
-    if (this.handlers.has(type)) this.handlers.get(type)(fn);
+    if (this.handlers.has(eventName)) this.handlers.get(eventName)(fn);
     
-    // Fire matching wildcard handlers
-    if (this.fullWildcardsSupport) {
-      this.wildcardHandlers.forEach(handlerObj => {
-        if (handlerObj.regexp.test(type)) handlerObj.handler(type, fn);
-      });      
-    }
+    // Fire special handlers
+    this.specialHandlers.forEach((specialHandler, specialHandlerEventName) => {
+      if (specialHandler.test(eventName)) specialHandler.handler(eventName, fn);
+    });   
     
-    // Or fire all handlers
-    else if (this.handlers.has('*')) {
-      this.handlers.get('*')(type, fn);
+    // Or fire all handlers if a '*' handler has been set
+    if (this.handlers.has('*')) {
+      this.handlers.get('*')(eventName, fn);
     }
   }
 
